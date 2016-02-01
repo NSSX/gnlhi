f53b16ac4eadaa5e460189b866135faceff08cd1
Anthony VELLA <avella@e3r6p5.42.fr>
2016-02-01 17:51:27 +0100

    l

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avella <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/06 19:00:47 by avella            #+#    #+#             */
+/*   Updated: 2016/01/06 19:02:23 by avella           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 30000000
+# define MAX_FD 256
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "libft.h"
+
+int get_next_line(int const fd, char **line);
+#endif
